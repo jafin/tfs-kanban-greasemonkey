785b538853a701bf0c48463ba3a0fe77b9d313c6
Jason Finch <jason.finch@gmail.com>
2014-02-27 12:14:59 +1000

    initial commit.

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 tfs-kanban-greasemonkey
 =======================
 
-tfs-kanban-greasemonkey
+This is a hacked up greasemonkey script for TFS2012 Web Kanban to enable colour coding cards by tag name.
+It also adds some story points total to the header bar by using the Effort value. (We use effort value for story points)
+
+![Example](doc/screenshot.jpg)
---
+++ js/tfs-kanban-greasemonkey.user.js
@@ -0,0 +1,122 @@
+// ==UserScript==
+// @name       TFS Task Board Enhancements
+// @namespace  jafin
+// @version    0.10
+// @description  Adds dynamic colouring to tfs tasks based on tags
+// @include    http://*/tfs*board
+// @grant       none
+// ==/UserScript==
+
+var $ = unsafeWindow.jQuery;
+
+// add a global css style.
+function addGlobalStyle(css) {
+    var head, style;
+    head = document.getElementsByTagName('head')[0];
+    if (!head) {
+        return;
+    }
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+}
+
+function isNumber(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function updateTasks() {
+    var swimlane = 0;
+    //count per lane.
+    $("div.member-content").each(function () {
+        var lanePoints = 0;
+        var $this = $(this);
+        $this.find("div.board-tile").each(function () {
+            var $this = $(this);
+            var text = $("div.value", $this).text();
+            if (isNumber(text)) {
+                lanePoints += parseInt(text);
+            }
+        });
+
+        //update column points
+        var selector = 'div.points-total:eq(' + swimlane + ')';
+        console.log('updating column points.');
+        if ($('div.points-total').length == 0) {
+            //tfs deleted our columns or they just don't exist yet..
+            $('div.member-header-content').append('<div class="points-total"></div>');
+        }
+        $(selector).text(lanePoints);
+        swimlane++;
+    });
+
+    //calculate some points
+    $("div.content-container >div.cell:last")
+        .find("div.board-tile").each(function () {
+            var $this = $(this);
+            var text = $("div.value", $this).text();
+            if (isNumber(text)) {
+                enhancedModel.storyPointsDone += parseInt(text);
+            }
+        });
+
+    //Points todo.
+    $("div.board-tile").each(function () {
+        var $this = $(this);
+        var text = $("div.value", $this).text();
+        if (isNumber(text)) {
+            enhancedModel.storyPointsTotal += parseInt(text);
+        }
+    });
+
+    $("div.board-tile").each(function () {
+        var $this = $(this);
+        var $title = $('.title', this);
+        var titleText = $title.text();
+        $('span._tspTagCounter', this).each(function () {
+            var $this = $(this);
+            var titles = $this.attr('title');
+            if (/BLOCKED/i.test(titles)) {
+                $this.parent().addClass('board-tile-blocked');
+            }
+            if (/DEFECT/i.test(titles)) {
+                $this.parent().addClass('board-tile-defect');
+            }
+        });
+
+
+        if (/DEFECT/i.test(titleText)) {
+            console.log('defect found');
+            $this.addClass('board-tile-defect');
+        } else {
+            $this.addClass('witTypeDevelopment');
+        }
+    });
+
+    $('#storyPointsContainer').html('Story points total: ' + '<span class="points">' + enhancedModel.storyPointsTotal + '</span>'
+        + ' Remaining: ' + '<span class="points">' + enhancedModel.storyPointsRemaining() + '</span>'
+        + ' Done: ' + '<span class="points">' + enhancedModel.storyPointsDone + '</span>');
+}
+
+addGlobalStyle('.board-tile-defect { border-left: 12px solid #FFAA00 !important; background-color: #FFD47F !important;}');
+addGlobalStyle('.board-tile-blocked { border-left: 12px solid #FF002A !important; background-color: #FFABB9 !important;}');
+addGlobalStyle('#storyPointsContainer { color:white; font-weight:bold; margin-left:20px; float:left; padding-left:4px; padding-right:4px; background-color:#08395E;}');
+addGlobalStyle('.points { color:yellow; font-weight:bold;}');
+addGlobalStyle('#imageHolder { position:fixed; left:400px; top:200px; width:600px; height:600px;}');
+addGlobalStyle('.points-total { color: #296401; float:right; background-color:#FBF2DC;padding-left:1px;padding-right:1px;}');
+
+var enhancedModel = {};
+enhancedModel.storyPointsDone = 0;
+enhancedModel.storyPointsTotal = 0;
+enhancedModel.storyPointsRemaining = function () {
+    return enhancedModel.storyPointsTotal - enhancedModel.storyPointsDone;
+};
+
+$(document).ready(function () {
+    $('div.member-header-content').append('<div class="points-total"></div>');
+    $('div#header-row').append('<div id="ultimateAwesome" style="color:white;margin-left:50px;float:left;">TFS KANBAN Enhancer</div>')
+        .append('<div id="storyPointsContainer"></div>');
+    $('body').append('<div id="imageHolder"></div>');
+    window.setTimeout(updateTasks, 5000);
+});
